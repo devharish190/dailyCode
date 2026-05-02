d545260c34c05f7b4b04b28934ae555d3a310265
Harish <khharish2@gmail.com>
2026-05-02 11:58:41 +0100

    leetcode daily challenges

---
+++ dailyCode/src/main/java/com/leetcode/dailychallenge/dailycode/BuyAndSell.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.leetcode.dailychallenge.dailycode;
+
+/**
+ *
+ * @author Admin
+ */
+public class BuyAndSell {
+
+    public static void main(String[] args) {
+        int arr[] = {7, 1, 5, 3, 6, 4};
+
+        int maxProfit = 0;
+
+        int minVal = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            minVal = Math.min(arr[i], minVal);
+            int profit = arr[i] - minVal;
+            maxProfit = Math.max(profit, maxProfit);
+        }
+        System.out.println(maxProfit);
+
+    }
+}
---
+++ dailyCode/src/main/java/com/leetcode/dailychallenge/dailycode/LeetCode788.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.leetcode.dailychallenge.dailycode;
+
+/**
+ *
+ * @author Admin
+ */
+public class LeetCode788 {
+
+    public static void main(String[] args) {
+
+        LeetCode788 l = new LeetCode788();
+
+        System.out.println(":::::::: " + l.rotatedDigits(10));
+
+    }
+
+    public int rotatedDigits(int n) {
+
+        int validCount = 0;
+
+        for (int i = 1; i <= n; i++) {
+            boolean isValid = true;
+            boolean isChanged = false;
+            int temp = i;
+            while (temp > 0) {
+                int lastDigit = temp % 10;
+                if (lastDigit == 3 || lastDigit == 4 || lastDigit == 7) {
+                    isValid = false;
+                    break;
+                } else if (lastDigit == 2 || lastDigit == 5 || lastDigit == 6 || lastDigit == 9) {
+                    isChanged = true;
+                }
+                temp = temp / 10;
+            }
+            if (isValid && isChanged) {
+                validCount++;
+            }
+        }
+        return validCount;
+
+    }
+}
